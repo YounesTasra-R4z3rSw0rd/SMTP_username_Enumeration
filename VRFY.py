e2f5eafb8b2d09d6a8bef8a95ae61ad534313894
Younes Tasra <101610095+YounesTasra-R4z3rSw0rd@users.noreply.github.com>
2023-02-02 01:41:53 +0100

    python script

---
+++ VRFY.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import socket
+import sys
+
+if len(sys.argv) != 2:
+	print ("Usage: vrfy.py <username>")
+	sys.exit(0)
+	
+# Create a Socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect to the Server
+connect = s.connect(('10.10.115.36',25))  # CHANGE THIS   
+
+# Receive the banner
+banner = s.recv(1024)
+print (banner)
+
+# VRFY a user
+username = bytes(sys.argv[1], "UTF-8")
+s.send(b'VRFY ' + username + b'\r\n')
+result = s.recv(1024)
+print (str(result))
+
+# Close the socket
+s.close()
